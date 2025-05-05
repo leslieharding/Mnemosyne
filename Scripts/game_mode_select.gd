ec18621f53848a1aa516493a811cf0bf60dd08de
leslieharding <141376020+leslieharding@users.noreply.github.com>
2025-05-05 18:31:26 +1200

    hermes artemis navigation

--- Scripts/game_mode_select.gd
+++ Scripts/game_mode_select.gd
@@ -7,3 +7,11 @@ func _on_apollo_button_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+func _on_hermes_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Hermes.tscn")
+
+
+func _on_artemis_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Artemis.tscn")
